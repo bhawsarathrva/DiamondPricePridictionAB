0c61706f1493bbcad1c52b595ddd360bc232fd00
Athrva <athrvabh124@gmail.com>
2024-03-27 18:00:54 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requiremets=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        return requirements
+
+
+setup(
+    name='DiamondPricePrediction',
+    version='0.0.1',
+    author='Athrva Bhawsar',
+    author_email='athrvabh125@gamil.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
